ff09b75f484614a003ec784bea1273aa6d3c007b
M Saiful Bari <sbmaruf@gmail.com>
2022-08-26 03:39:45 +0800

    ckpt

--- evaluation/run_eval_xcopa_05.sh
+++ evaluation/run_eval_xcopa_05.sh
@@ -20,8 +20,8 @@ for MODEL_SIGNATURE in "tr13f-6b3-ml-t0-lmtoks341b-t0toks13b-xp3capmix" "tr13f-6
     MODEL_NAME_OR_PATH="bigscience/$MODEL_SIGNATURE"
     for TEMPLATE_NAME in "exercise" "…What could happen next, C1 or C2?" "i_am_hesitating" "plausible_alternatives" "C1 or C2? premise, so/because…" "…As a result, C1 or C2?" "best_option" "…which may be caused by" "more likely" "cause_effect" "…why? C1 or C2" "choose" ; 
         do
-	TEMPLATE_CONFIG_FLAG=${TEMPLATE_CONFIG_NAME//[\/ ]/_}
-	TEMPLATE_FLAG=${TEMPLATE_NAME//[\/ ]/_}
+        TEMPLATE_CONFIG_FLAG=${TEMPLATE_CONFIG_NAME//[\/ ]/_}
+        TEMPLATE_FLAG=${TEMPLATE_NAME//[\/ ]/_}
         OUTPUT_DIR="dumped"/$MODEL_SIGNATURE/$DATASET_NAME"_"$DATASET_CONFIG_NAME"_"$TEMPLATE_CONFIG_FLAG"_"$TEMPLATE_FLAG
         if [ ! -d $OUTPUT_DIR ] 
             then
@@ -47,7 +47,7 @@ for MODEL_SIGNATURE in "tr13f-6b3-ml-t0-lmtoks341b-t0toks13b-xp3capmix" "tr13f-6
             TEMPLATE_CONFIG_FLAG=${TEMPLATE_CONFIG_NAME//[\/ ]/_}
             TEMPLATE_FLAG=${TEMPLATE_NAME//[\/ ]/_}
             OUTPUT_DIR="dumped"/$MODEL_SIGNATURE/$DATASET_NAME"_"$DATASET_CONFIG_NAME"_"$TEMPLATE_CONFIG_FLAG"_"$TEMPLATE_FLAG
-	    if [ ! -d $OUTPUT_DIR ] 
+	        if [ ! -d $OUTPUT_DIR ] 
                 then
                 mkdir -p $OUTPUT_DIR
                 python run_eval.py \
